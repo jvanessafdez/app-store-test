798c6d9ddce98379b5c9229ed055a8e27eefa1c1
vanessa fernandez <jvanessafdez@gmail.com>
2023-11-13 07:12:13 -0500

    fix: product list screen

--- src/modules/home/presentation/screens/ProoductListScreen.tsx
+++ src/modules/home/presentation/screens/ProoductListScreen.tsx
@@ -1,51 +1,27 @@
 import React, { useState } from 'react';
-import { SafeAreaView, ScrollView, StatusBar, StatusBarStyle, Text, Image } from 'react-native';
-import { View } from 'react-native';
+import { 
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  View
+} from 'react-native';
 import general from '../../../../assets/css/general';
 import { CustomButton } from '../../../../components/common/Button/button';
-import { useNavigation } from '@react-navigation/native';
-import { SmallCard } from '../../../../components/common/SmallCard/smallCard';
-import sport_elements from '../../../../assets/images/sport_elements.png'
-import clothes from '../../../../assets/images/clothes.png'
-import shoes from '../../../../assets/images/shoes.png'
+import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { colors } from '../../../../assets/css/colors';
 import { arrayProducts } from '../../../../../mocks/elements';
 import { ECategory, Product } from '../../domain/product';
 import { ProductCard } from '../components/productCard';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { CardImage } from '../components/cardImage';
+import { RootStackParamList } from '../../../../navigation/navigationTypes';
 
 const STYLES = ['default', 'dark-content', 'light-content'] as const;
 
 function ProductListScreen() {
-  const navigation = useNavigation()
-  const [category, setCategory] = useState(ECategory.ELEMENT)
-  const [statusBarStyle, setStatusBarStyle] = useState<StatusBarStyle>(
-    STYLES[1],
-  );
-
-  interface CardProps {
-    color?: string;
-    image: string;
-    label: string;
-    category: string;
-  }
-
-  const CardImage = ({color, image, label, category}: CardProps) => {
-    return(
-      <TouchableOpacity onPress={() => setCategory(category)}>
-      <SmallCard
-        width={110}
-        color = {color && color}
-      >
-        <Image
-          source={image}
-          style={general.stretch}
-        />
-        <Text style={[general.normalText, {textAlign: 'center'}]}>{label}</Text>
-      </SmallCard>
-      </TouchableOpacity>
-    )
-  }
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const [category, setCategory] = useState<ECategory>(ECategory.ELEMENT);
+  const statusBarStyle = STYLES[1];
 
   return (
     <SafeAreaView style={general.screen}>
@@ -56,9 +32,23 @@ function ProductListScreen() {
       <View style={{height:'23%'}}>
         <Text style={[general.subtitle, general.marginLeft20]}>Categorías</Text>
         <View style={[general.rowSpace, {width:'95%', alignSelf:'flex-end'}]}>
-          <CardImage image={sport_elements} label={'Elementos\nDeportivos'} category={ECategory.ELEMENT}/>
-          <CardImage color={colors.category2} image={clothes} label={'Ropa\nDeportiva'} category={ECategory.CLOTHES}/>
-          <CardImage color={colors.category3} image={shoes} label={'Zapatos\nDeportivos'} category={ECategory.SHOES}/>
+          <CardImage 
+            label={'Elementos\nDeportivos'}
+            category={ECategory.ELEMENT}
+            setCategory={setCategory}
+          />
+          <CardImage 
+            color={colors.category2}
+            label={'Ropa\nDeportiva'}
+            category={ECategory.CLOTHES}
+            setCategory={setCategory}
+          />
+          <CardImage
+            color={colors.category3}
+            label={'Zapatos\nDeportivos'}
+            category={ECategory.SHOES}
+            setCategory={setCategory}
+            />
         </View>
       </View>
       <ScrollView style={{height:'67%'}}>
